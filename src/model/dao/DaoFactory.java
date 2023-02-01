8d71c95ee9a1fbb86975400899eb1731a3dc0620
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-01 20:25:31 -0300

    Método findById implementado

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDAO() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }
