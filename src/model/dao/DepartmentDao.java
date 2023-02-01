1e5b3d1a0a0ba0af0b5290ec4700f809f40af26a
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-01 19:59:18 -0300

    SellerDaoJDBC e DaoFactory implementados

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -4,7 +4,7 @@ import model.entities.Department;
 
 import java.util.List;
 
-public interface DepartmentDAO {
+public interface DepartmentDao {
 
     void insert(Department obj);
     void update(Department obj);
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -4,7 +4,7 @@ import model.entities.Seller;
 
 import java.util.List;
 
-public interface SellerDAO {
+public interface SellerDao {
 
     void insert(Seller obj);
     void update(Seller obj);
