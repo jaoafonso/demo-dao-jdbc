12a24f18adcc44a29eced42f093be2f542b1d5b1
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-02-01 22:35:00 -0300

    Método findAll implementado

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,5 +21,9 @@ public class Program {
         Department department = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(department);
         list.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        list.forEach(System.out::println);
     }
 }
